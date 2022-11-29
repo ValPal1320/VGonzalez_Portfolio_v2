9f95625b230fbf4d045b4f02e9d1dc3a3627d9b0
Val Pal <valgonzalez13@yahoo.com>
2022-11-29 00:03:11 -0600

    portfolio logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import Nav from 'react-bootstrap/Nav';
 import './Navbar.css';
+import Logo from '../assets/imgs/vgon.png'
 
 function Navbar() {
     return (
         <>
             <Nav className="justify-content-end navBar" activeKey="/home">
+                <img src={Logo} alt='logo'></img>
                 <Nav.Item>
                     <Nav.Link href="/home" id="navText">About Me</Nav.Link>
                 </Nav.Item>
