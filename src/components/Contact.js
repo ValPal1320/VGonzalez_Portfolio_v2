e2f1fdd2d6779b65135760ac65392433ef2b8454
Val Pal <valgonzalez13@yahoo.com>
2022-12-02 22:05:17 -0600

    contact form completed, work on portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,27 +12,30 @@ function Contact() {
                 <Card.Title className="contactMe">
                     <h1>Contact Me</h1>
                 </Card.Title>
-                <Form.Group className="mb-3" controlId="formName" id="formContent">
-                    <Form.Label>Full Name:</Form.Label>
-                    <Form.Control type="name" placeholder="Enter full name" />
-                </Form.Group>
+                <div id="form">
+                    <Form.Group className="mb-3" controlId="formName" id="formContent">
+                        <Form.Label id="label">Full Name:</Form.Label>
+                        <Form.Control id="inputBox" type="name" placeholder="Enter full name" />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail" id="formContent">
-                    <Form.Label>Email address:</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        I'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail" id="formContent">
+                        <Form.Label id="label">Email address:</Form.Label>
+                        <Form.Control id="inputBox" type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            I'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-                <InputGroup id="messageBox">
-                    <Form.Label>Message:</Form.Label>
-                    <Form.Control as="textarea" aria-label="With textarea" />
-                </InputGroup>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                    <InputGroup id="messageBox">
+                        <Form.Label id="label">Message:</Form.Label>
+                        <Form.Control id="message" as="textarea" aria-label="With textarea" />
+                    </InputGroup>
+                </div>
+                <div id="btn">
+                    <Button id="submitBtn" variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </div>
             </div>
         </Form >
     );
