220d88f56ddfd79902f2d007189ade27ae63a37d
Val Pal <valgonzalez13@yahoo.com>
2022-12-04 22:40:44 -0600

    links now work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,12 @@ import Button from 'react-bootstrap/Button';
 import './Navbar.css';
 import Logo from '../assets/imgs/vgon.png'
 import resume from '../assets/imgs/V_Gonzalez_Resume_2022.pdf';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <>
-            <Nav className="justify-content-end navBar" activeKey="/about-me">
+            {/* <Nav className="justify-content-end navBar" activeKey="/about-me">
 
                 <a href="/"><img src={Logo} alt='logo'></img></a>
 
@@ -38,6 +39,24 @@ function Navbar() {
                             Contact
                         </Button>
                     </a>
+                </Nav.Item>
+            </Nav> */}
+
+            <Nav className="justify-content-end navBar" activeKey="/about-me">
+
+                <a href="/"><img src={Logo} alt='logo'></img></a>
+
+                <Nav.Item>
+                    <Link id='navText' to="/about-me">About Me</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link id="navText" to="/portfolio">Portfolio</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link id="navText" to="{resume}">Resume</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link id="navText" to="/contact">Contact</Link>
                 </Nav.Item>
             </Nav>
         </>
